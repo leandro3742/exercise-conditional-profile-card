ea59f06c8d4119553f701c297128b4cc7f52ef2e
leandro3742 <leandro.marrero@outlook.com>
2021-04-20 13:53:44 +0000

    trabajo terminado

--- src/js/index.js
+++ src/js/index.js
@@ -44,9 +44,9 @@ function render(variables = {}) {
           <h3>${variables.country}  ${variables.city}</h3>
           <ul class="${variables.socialMediaPosition}">
             <li><a href="${variables.twitter}"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="${variables.twitter}"><i class="fa fa-github"></i></a></li>
-            <li><a href="${variables.twitter}"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="${variables.twitter}"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="${variables.github}"><i class="fa fa-github"></i></a></li>
+            <li><a href="${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="${variables.instagram}"><i class="fa fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
